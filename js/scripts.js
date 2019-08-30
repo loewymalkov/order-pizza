369472a82cf03bd194b687e6bb35e61bb711de32
Loewy Malkovich <loewymalkov@gmail.com>
2019-08-30 13:52:09 -0700

    modify prototypes for size and topping choices and create master prototype to create pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,30 @@
-function Pizza(size, topping) {
-  this.size = size;
-  this.topping = topping;
-  this.price = 0;
+function Pizza(sizeChoice, toppingChoice, totalPrice) {
+  this.size = sizeChoice;
+  this.topping = toppingChoice;
+  this.price = totalPrice;
 }
 
 // Pizza size constructors
 function Small() {
   this.name = "Small";
-  this.price = 5;
 };
 function Medium() {
   this.name = "Medium",
-   this.price = 7;
 };
 function Large() {
   this.name = "Large",
-   this.price = 7;
 }
 
 // Toppings constructors
 function Sausage() {
   this.name = "Sausage";
-  this.price = 2;
 }
 function Anchovy() {
   this.name = "Anchovy";
-  this.price = 2;
 };
 
 function Onion() {
   this.name = "Onion";
-  this.price = 1;
 };
  
 
@@ -39,9 +33,20 @@ let smallChoice = new Small()
 let toppingChoice = new Topping()
 let pizza = new Pizza(smallChoice, sausageChoice)
 
+Pizza.prototype.makePizza() {
+  pizza.chooseSize();
+  pizza.chooseTopping();
+};
 
-Pizza.prototype.makePizza = function() {
-  if (this.size === smallChoice {
-    this.price += smallChoice.price;
+Pizza.prototype.chooseSize = function(sizePrice) {
+  if (this.size === smallChoice) {
+    this.price += sizePrice.val();
   }
 }
+
+Pizza.prototype.chooseTopping = function(toppingPrice) {
+  if (this.topping === sausageChoice) {
+    this.price += toppingPrice.val();
+  }
+}
+
