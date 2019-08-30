3de42ef705bf9cb8c73b762b8719c7a20bf1fec9
Loewy Malkovich <loewymalkov@gmail.com>
2019-08-30 12:20:16 -0700

    this will add constructors to deal with size and topping types, and include price for each object

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,47 @@
-function Pizza() {
-  this.size = ["S", "M", "L"];
-  this.topping = ["Anchovies", "Onions", "Sausage"];
+function Pizza(size, topping) {
+  this.size = size;
+  this.topping = topping;
+  this.price = 0;
 }
 
+// Pizza size constructors
+function Small() {
+  this.name = "Small";
+  this.price = 5;
+};
+function Medium() {
+  this.name = "Medium",
+   this.price = 7;
+};
+function Large() {
+  this.name = "Large",
+   this.price = 7;
+}
+
+// Toppings constructors
+function Sausage() {
+  this.name = "Sausage";
+  this.price = 2;
+}
+function Anchovy() {
+  this.name = "Anchovy";
+  this.price = 2;
+};
+
+function Onion() {
+  this.name = "Onion";
+  this.price = 1;
+};
+ 
+
+
+let smallChoice = new Small()
+let toppingChoice = new Topping()
+let pizza = new Pizza(smallChoice, sausageChoice)
+
+
 Pizza.prototype.makePizza = function() {
-  
+  if (this.size === smallChoice {
+    this.price += smallChoice.price;
+  }
 }
